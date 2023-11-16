7a6258e536b29367aad505a750f87e7f7197591d
hneff520 <hneff@unomaha.edu>
2023-11-16 12:49:13 -0600

    television.py commit

--- television.py
+++ television.py
@@ -1,8 +1,8 @@
 class Television:
-    MIN_VOLUME = 0
-    MAX_VOLUME = 2
-    MIN_CHANNEL = 0
-    MAX_CHANNEL = 3
+    MIN_VOLUME: int = 0
+    MAX_VOLUME: int = 2
+    MIN_CHANNEL: int = 0
+    MAX_CHANNEL: int = 3
 
     def __init__(self):
         self.__status: bool = False
@@ -10,28 +10,48 @@ class Television:
         self.__volume: int = Television.MIN_VOLUME
         self.__channel: int = Television.MIN_CHANNEL
 
-    def power(self):
+    def power(self) -> None:
+        '''
+        Toggles the __status variable of the Television to True or False
+        :return:
+        '''
         self.__status = not self.__status
 
-    def mute(self):
+    def mute(self) -> None:
+        '''
+        Toggles the __muted variable of the Television to True or False if __status is true
+        :return:
+        '''
         if self.__status:
             self.__muted = not self.__muted
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        '''
+        If __status is True, Increments __channel by 1, if __channel is at the MAX_CHANNEL value when channel_up is called __channel becomes the MIN_CHANNEL value
+        :return:
+        '''
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
             else:
                 self.__channel += 1
 
-    def channel_down(self):
+    def channel_down(self) -> None:
+        '''
+        If __status is True, Increments __channel by -1, if __channel is at the MIN_CHANNEL values when channel_down is called __channel becomes the MAX_CHANNEL value
+        :return:
+        '''
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
             else:
                 self.__channel -= 1
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        '''
+        If __status is True, sets __muted to false and Increments __volume by 1 if __volume is not at the MAX_VOLUME value
+        :return:
+        '''
         if self.__status:
             self.__muted = False
             if self.__volume != Television.MAX_VOLUME:
@@ -39,7 +59,11 @@ class Television:
             else:
                 self.__volume = Television.MAX_VOLUME
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        '''
+        If __status is True, sets __muted to false and Increments __volume by -1 if __volume is not at the MAX_VOLUME value
+        :return:
+        '''
         if self.__status:
             self.__muted = False
             if self.__volume != Television.MIN_VOLUME:
@@ -47,10 +71,11 @@ class Television:
             else:
                 self.__volume = Television.MIN_VOLUME
 
-    def get_volume(self):
-        pass
-
-    def __str__(self):
+    def __str__(self) -> str:
+        '''
+        returns the attributes of the Television object inside a string and is called whenever the object is put inside a print function
+        :return: Television object in string form
+        '''
         if self.__muted:
             return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {0}'
         else:
